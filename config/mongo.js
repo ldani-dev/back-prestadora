5761be003cda638f7721c551ccdcae6073095230
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-06-15 19:53:31 -0500

    add cluster from mongodb and put static mongo-seeding

--- config/mongo.js
+++ config/mongo.js
@@ -1,42 +1,44 @@
 const mongoose = require('mongoose')
 const DB_URL = process.env.MONGO_URI
+const MONGO_TEST = process.env.MONGO_TEST
+
 const loadModels = require('../app/models')
-
+const dbType = (process.env.NODE_ENV !== 'test') ? DB_URL : MONGO_TEST
+console.log(dbType)
 module.exports = () => {
-  const connect = () => {
-    mongoose.Promise = global.Promise
+    const connect = () => {
+        mongoose.Promise = global.Promise
 
-    mongoose.connect(
-      DB_URL,
-      {
-        keepAlive: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      },
-      (err) => {
-        let dbStatus = ''
-        if (err) {
-          dbStatus = `*    Error connecting to DB: ${err}\n****************************\n`
-        }
-        dbStatus = `*    DB Connection: OK\n****************************\n`
-        if (process.env.NODE_ENV !== 'test') {
-          // Prints initialization
-          console.log('****************************')
-          console.log('*    Starting Server')
-          console.log(`*    Port: ${process.env.PORT || 3000}`)
-          console.log(`*    NODE_ENV: ${process.env.NODE_ENV}`)
-          console.log(`*    Database: MongoDB`)
-          console.log(dbStatus)
-        }
-      }
-    )
-    mongoose.set('useCreateIndex', true)
-    mongoose.set('useFindAndModify', false)
-  }
-  connect()
+        mongoose.connect(
+            dbType, {
+                keepAlive: true,
+                reconnectTries: Number.MAX_VALUE,
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            },
+            err => {
+                let dbStatus = ''
+                if (err) {
+                    dbStatus = `*    Error connecting to DB: ${err}\n****************************\n`
+                }
+                dbStatus = `*    DB Connection: OK\n****************************\n`
+                    // Prints initialization
+                console.log('****************************')
+                console.log('*    Starting Server')
+                console.log(`*    Port: ${process.env.PORT || 3000}`)
+                console.log(`*    NODE_ENV: ${process.env.NODE_ENV}`)
+                console.log(`*    WS_SOCKET: ${process.env.WS_PORT || 5000}`)
+                console.log(`*    Database: MongoDB`)
+                console.log(dbStatus)
+            }
+        )
+        mongoose.set('useCreateIndex', true)
+        mongoose.set('useFindAndModify', false)
+    }
+    connect()
 
-  mongoose.connection.on('error', console.log)
-  mongoose.connection.on('disconnected', connect)
+    mongoose.connection.on('error', console.log)
+    mongoose.connection.on('disconnected', connect)
 
-  loadModels()
+    loadModels()
 }
--- seed.js
+++ seed.js
@@ -1,23 +1,26 @@
 require('dotenv-safe').config()
 const { Seeder } = require('mongo-seeding')
 const path = require('path')
+const DB_URL = process.env.MONGO_URI
+const MONGO_TEST = process.env.MONGO_TEST
+const dbType = (process.env.NODE_ENV !== 'test') ? DB_URL : MONGO_TEST
 const config = {
-  database: process.env.MONGO_URI,
-  inputPath: path.resolve(__dirname, './data'),
-  dropDatabase: false
+    database: dbType,
+    inputPath: path.resolve(__dirname, './data'),
+    dropDatabase: false
 }
 const seeder = new Seeder(config)
 const collections = seeder.readCollectionsFromPath(path.resolve('./data'))
 
-const main = async () => {
-  try {
-    await seeder.import(collections)
-    console.log('Seed complete!')
-    process.exit(0)
-  } catch (err) {
-    console.log(err)
-    process.exit(0)
-  }
+const main = async() => {
+    try {
+        await seeder.import(collections)
+        console.log('Seed complete!')
+        process.exit(0)
+    } catch (err) {
+        console.log(err)
+        process.exit(0)
+    }
 }
 
 main()
