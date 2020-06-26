0024248b689db1ee295846abf4e5b938e978c4d0
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-06-25 19:22:53 -0500

    create model clients

--- app/models/clients.js
+++ app/models/clients.js
@@ -2,87 +2,99 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const mongoosePaginate = require('mongoose-paginate-v2')
 const mongoose_delete = require('mongoose-delete')
-var aggregatePaginate = require('mongoose-aggregate-paginate-v2');
-const clientsSchema = new mongoose.Schema({
-    name: { // 1 
-        type: String,
-        required: true
+const aggregatePaginate = require('mongoose-aggregate-paginate-v2')
+const clientsSchema = new mongoose.Schema(
+  {
+    name: {
+      // 1
+      type: String,
+      required: true
     },
-    lastName: { //1
-        type: String,
-        required: true
+    lastName: {
+      // 1
+      type: String,
+      required: true
     },
-    phone: { //1
-        type: String,
-        required: true
+    phone: {
+      // 1
+      type: String,
+      required: true
     },
-    address: { //1
-        type: String,
-        required: true
+    address: {
+      // 1
+      type: String,
+      required: true
     },
-    document: { //1
-        type: String,
-        required: true
+    document: {
+      // 1
+      type: String,
+      required: true
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
-    interes: { //2
-        type: Number,
-        required: true
+    interes: {
+      // 2
+      type: Number,
+      required: true
     },
-    amount: { //2
-        type: Number,
-        required: true
+    amount: {
+      // 2
+      type: Number,
+      required: true
     },
     dateBegin: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true
     },
-    status: { //2
-        type: String,
-        enum: ['pending', 'completed', 'legalAction', 'lost'],
-        default: 'pending',
-        required: true
+    status: {
+      // 2
+      type: String,
+      enum: ['pending', 'completed', 'legalAction', 'lost'],
+      default: 'pending',
+      required: true
     },
-    jobRank: { //2
-        type: String,
-        required: true
+    jobRank: {
+      // 2
+      type: String,
+      required: true
     },
     addressJob: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     sectionJob: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     references: {
-        type: Array,
-        required: true
+      type: Array,
+      required: true
     },
     payments: {
-        type: Array,
+      type: Array
     },
     customData: {
-        type: Object
+      type: Object
     }
-}, {
+  },
+  {
     versionKey: false,
     timestamps: true
-})
+  }
+)
 
 clientsSchema.post('save', () => {
-    console.log('INSERCION EN clientss')
+  console.log('INSERCION EN clientss')
 })
 
-clientsSchema.pre('findOneAndRemove', async() => {
-    console.log('DELETE EN clientss')
+clientsSchema.pre('findOneAndRemove', async () => {
+  console.log('DELETE EN clientss')
 })
 
-clientsSchema.post('findOneAndUpdate', async() => {
-    console.log('ACTUALIZACION EN clientss')
+clientsSchema.post('findOneAndUpdate', async () => {
+  console.log('ACTUALIZACION EN clientss')
 })
 
 clientsSchema.plugin(mongoose_delete)
